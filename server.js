4b7414df4977432ad7dc8aadfae954d3173e7995
Shashikant Yashavant Lavhate <131379313+Shashikant-Lavhate@users.noreply.github.com>
2023-07-28 01:15:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/contact', function (req, res) {
     res.sendFile(path.join(___dirname + '/contact.html'));
 });
 
-var server = app.listen(3333, function () {
+var server = app.listen(3336, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://localhost:3336", host, port)
