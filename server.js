6961abe3b74b7acafa6fbe98a6a8f710dea834f6
Shashikant Yashavant Lavhate <131379313+Shashikant-Lavhate@users.noreply.github.com>
2023-07-28 00:53:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 
 
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(___dirname + '/index.html'));
